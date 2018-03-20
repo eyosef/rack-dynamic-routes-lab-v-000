ceaa090d0e29ac5ca5f06f50808ba8760d46b745
Eden Mesfun <emesfun@gmail.com>
2018-03-20 16:53:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,15 @@ class Application
     if !req.path.match("/items/")
       resp.status = 404
       resp.write "Route not found"
-    elsif 
+    elsif req.path.match("/items/")
+      req.path.match("/items/")
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+
+      if item == nil
+        resp.status = 400 
+      end
+      
     else req.path.match("/items/")
       req.path.match("/items/")
       item_name = req.path.split("/items/").last
