a6edf94569331eed3fbd19e869b6ac9ecfea5b80
Eden Mesfun <emesfun@gmail.com>
2018-03-20 16:55:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,6 @@ class Application
       req.path.match("/items/")
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-
       if item == nil
         resp.status = 400
       else
